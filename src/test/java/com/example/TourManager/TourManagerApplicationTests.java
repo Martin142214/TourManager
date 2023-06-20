36c3c3d104d1d01094a72b168f26f407fad2c698
Martin142214 <mi_kalonkin@mail.bg>
2023-06-20 12:04:12 +0300

    page templates

---
+++ src/test/java/com/example/TourManager/TourManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TourManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
