037cb917b6df433739fc2200187018a6a0737d93
Martin142214 <mi_kalonkin@mail.bg>
2023-07-19 17:21:52 +0300

    add js validation for multiple images input

--- src/main/resources/static/scripts/images_input_validation.js
+++ src/main/resources/static/scripts/images_input_validation.js
@@ -1,10 +1,40 @@
-window.addEventListener("load", function () {
-    if ($("#manyImagesInput")[0].files.length <= 0 || $("#manyImagesInput")[0].files.length > 5) {
-        $(".submitBtn").css('pointer-events', 'none');
+var manyImagesInput = document.getElementById("manyImagesInputCreate");
+
+manyImagesInput.addEventListener("change", function () {
+    if ($("#manyImagesInputCreate")[0].files.length == 0 || $("#manyImagesInputCreate")[0].files.length > 5) {
+        $(".submitBtnCreate").css('pointer-events', 'none');
     } else {
-        $(".submitBtn").css('pointer-events', 'all');
+        $(".submitBtnCreate").css('pointer-events', 'all');
     }
 });
+
+/*setInterval(
+    $(function() {
+        var // Define maximum number of files.
+            max_file_number = 5,
+            // Define your form id or class or just tag.
+            $form = $('#landmarkCreateModalForm'),
+            // Define your upload field class or id or tag.
+            $file_upload = $('#manyImagesInputCreate', $form),
+            // Define your submit class or id or tag.
+            $button = $('#submitBtnCreate', $form);
+      
+        // Disable submit button on page ready.
+        $button.prop('disabled', 'disabled');
+      
+        $file_upload.on('change', function () {
+          var number_of_images = $(this)[0].files.length;
+          if (number_of_images > max_file_number) {
+            alert(`You can upload maximum ${max_file_number} files.`);
+            $(this).val('');
+            $button.prop('disabled', 'disabled');
+          } else {
+            $button.prop('disabled', false);
+          }
+        });
+      }), 100
+
+)*/
 //TODO
 // this function is not working properly
 // it loads the function only ones
