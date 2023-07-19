69d7b26ce82920d2a5dc54fa5024da832cc919f2
Martin142214 <mi_kalonkin@mail.bg>
2023-07-19 12:55:50 +0300

    create a copyDirectory() in FileUploadUtils class -- transfer files from one location(source) to another(destination)

--- src/main/java/com/example/TourManager/FileUploadUtils.java
+++ src/main/java/com/example/TourManager/FileUploadUtils.java
@@ -1,7 +1,11 @@
 package com.example.TourManager;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -21,5 +25,34 @@ public class FileUploadUtils {
         InputStream inputStream = multipartFile.getInputStream();
         Path filePath = uploadPath.resolve(fileName);
         Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);     
+    }
+
+    public static void copyDirectory(File sourceLocation , File targetLocation)
+    throws IOException {
+
+        if (sourceLocation.isDirectory()) {
+            if (!targetLocation.exists()) {
+                targetLocation.mkdir();
+            }
+
+            String[] children = sourceLocation.list();
+            for (int i=0; i<children.length; i++) {
+                copyDirectory(new File(sourceLocation, children[i]),
+                        new File(targetLocation, children[i]));
+            }
+        } else {
+
+            InputStream in = new FileInputStream(sourceLocation);
+            OutputStream out = new FileOutputStream(targetLocation);
+
+            // Copy the bits from instream to outstream
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
+        }
     }
 }
