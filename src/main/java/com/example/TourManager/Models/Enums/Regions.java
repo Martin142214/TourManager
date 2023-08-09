c1d89ee9cd342594ab7c76c1cc67fb6c078ff9a2
Martin142214 <mi_kalonkin@mail.bg>
2023-08-09 15:00:29 +0300

    re-construct historical "Regions" enum -- add english keys and bulgarian names to enum -- set getName() for every name value

--- src/main/java/com/example/TourManager/Models/Enums/Regions.java
+++ src/main/java/com/example/TourManager/Models/Enums/Regions.java
@@ -1,11 +1,21 @@
 package com.example.TourManager.Models.Enums;
 
 public enum Regions {
-    Югоизточна_България,
-    Стара_планина,
-    Софийско,
-    Североизточна_България,
-    Северозападна_България,
-    Рила_планина,
-    Родопи
+    RUINS ("Крепости_и_руини"),
+    SANCTUARIES ("Светилища"),
+    THRACIAN ("Тракийски"),
+    ROMAN ("Римски"),
+    MEDIEVAL ("Средновековни"),
+    VAZROZHDENSKY ("Възрожденски"),
+    TOMBS ("Гробници");
+
+    private String name;
+
+    Regions(String name) {
+        this.name = name;
+    }
+
+    public String getHistoricalRegionName() {
+        return name;
+    }
 }
