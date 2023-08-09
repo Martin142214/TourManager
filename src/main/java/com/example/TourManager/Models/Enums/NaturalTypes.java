05fc32fa50a43cc3131765f97575147315debcdb
Martin142214 <mi_kalonkin@mail.bg>
2023-08-09 14:59:58 +0300

    re-construct "NaturalTypes" enum -- add english keys and bulgarian names to enum -- set getName() for every name value

--- src/main/java/com/example/TourManager/Models/Enums/NaturalTypes.java
+++ src/main/java/com/example/TourManager/Models/Enums/NaturalTypes.java
@@ -1,11 +1,22 @@
 package com.example.TourManager.Models.Enums;
 
 public enum NaturalTypes {
-    Крепости_и_руини,
-    Светилища,
-    Тракийски,
-    Римски,
-    Средновековни,
-    Възрожденски,
-    Гробници
+    SOUTH_EAST ("Югоизточна_България"),
+    SOUTH_WEST ("Югозападна_България"),
+    OLD_MOUNTAIN ("Стара_планина"),
+    SOFIA_REGION ("Софийско"),
+    NORTH_EAST ("Североизточна_България"),
+    NORTH_WEST ("Северозападна_България"),
+    RILA_MOUNTAIN ("Рила_планина"),
+    RODOPI_MOUNTAIN ("Родопи");
+
+    private String name;
+
+    NaturalTypes(String name) {
+        this.name = name;
+    }
+
+    public String getNaturalTypeName() {
+        return name;
+    }
 }
